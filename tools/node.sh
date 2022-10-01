9e2fe6b164cc8d2768258f72ca416d87f631de6b
Jens Awisus <awisus.jens@gmail.com>
2022-10-01 14:26:39 +0200

    setup macOS

---
+++ tools/node.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf $HOME/.nvm
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm install --lts

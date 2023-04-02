1f79eb03dc6228fa7a78b16de916aa27f3280336
Jens Awisus <awisus@users.noreply.github.com>
2023-04-02 16:05:47 +0200

    add persistent app

--- install.sh
+++ install.sh
@@ -78,7 +78,7 @@ defaults write com.apple.dock orientation left
 defaults write com.apple.dock mineffect -string scale
 defaults write com.apple.dock show-recents -bool no
 defaults write com.apple.dock persistent-apps -array
-for dockItem in /Applications/{"Signal","Thunderbird","Firefox","IntelliJ IDEA CE","Fork","iTerm"}.app; do
+for dockItem in /Applications/{"Signal","Thunderbird","Firefox","IntelliJ IDEA CE","Visual Studio Code","Fork","iTerm"}.app; do
   defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>'$dockItem'</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 done
 killall Dock

4806047eb388cd9a64c01bf4da876c1ea9f648a8
Jens Awisus <awisus@users.noreply.github.com>
2023-04-02 14:05:13 +0200

    reset persistent apps after update

--- update.sh
+++ update.sh
@@ -23,3 +23,9 @@ echo "done."
 echo ""
 
 print_status "SUCCESS"
+
+defaults write com.apple.dock persistent-apps -array
+for dockItem in /Applications/{"Signal","Thunderbird","Firefox","IntelliJ IDEA CE","Fork","iTerm"}.app; do
+  defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>'$dockItem'</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+done
+killall Dock

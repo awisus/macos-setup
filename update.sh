253b4998d31e23c467d5a8570a8e4a9a642365d0
Jens Awisus <awisus@users.noreply.github.com>
2023-04-06 10:18:55 +0200

    restore dock items

--- update.sh
+++ update.sh
@@ -8,6 +8,18 @@ function wrap_dock_item() {
   echo '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>'$1'</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 }
 
+function restore_persistent_apps() {
+  defaults write com.apple.dock persistent-apps -array
+  for dockItem in $persistentApps; do
+    dockItem=$(echo $dockItem | sed "s/file:\/\///g" | sed "s/\/\";//g" | sed "s/%20/ /g")
+    defaults write com.apple.dock persistent-apps -array-add "$(wrap_dock_item "$dockItem")"
+  done
+
+  killall Dock
+}
+
+persistentApps=($(defaults read com.apple.dock persistent-apps | grep -o 'file://.*;'))
+
 print_status "GAINING GROUP OWNERSHIP"
 sudo chgrp staff   /Applications/*
 sudo chmod g+w     /Applications/*
@@ -26,10 +38,9 @@ sudo xattr -d com.apple.quarantine /Applications/*
 echo "done."
 echo ""
 
-print_status "SUCCESS"
+print_status "RESET DOCK ITEMS"
+restore_persistent_apps
+echo "done."
+echo ""
 
-defaults write com.apple.dock persistent-apps -array
-for dockItem in /Applications/{"Signal","Firefox","Brave Browser","Thunderbird","Visual Studio Code","IntelliJ IDEA CE","Fork","iTerm","NVIDIA GeForce NOW"}.app; do
-  defaults write com.apple.dock persistent-apps -array-add "$(wrap_dock_item $dockItem)"
-done
-killall Dock
+print_status "SUCCESS"
